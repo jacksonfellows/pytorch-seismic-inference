4e99adb564a06ba182c9708741e50e5c14e7db73
Jackson Fellows <jtfellows@icloud.com>
2024-04-29 14:03:13 -0400

    Update download dir

--- mt_st_helens_download_config.py
+++ mt_st_helens_download_config.py
@@ -3,7 +3,7 @@ from obspy import UTCDateTime
 starttime = UTCDateTime(2000, 1, 1)
 endtime = UTCDateTime(2024, 1, 1)
 
-download_dir = "~/mt_st_helens"
+download_dir = "/share/barcheck/data/seismic/pnw_surface_events/mt_st_helens_2000_2024"
 
 # Mt. St. Helens crater
 lat = 46.203880
