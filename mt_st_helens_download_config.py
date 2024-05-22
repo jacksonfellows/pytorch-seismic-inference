a7fac55bcd22ceb6aa312c30db69e0bb60fb6bd8
Jackson Fellows <jtfellows@icloud.com>
2024-05-22 11:36:34 -0700

    Download config for Mt. St. Helens

---
+++ mt_st_helens_download_config.py
@@ -0,0 +1,16 @@
+from obspy import UTCDateTime
+
+starttime = UTCDateTime(2000, 1, 1)
+endtime = UTCDateTime(2024, 1, 1)
+
+download_dir = "/share/barcheck/data/seismic/pnw_surface_events/mt_st_helens_2000_2024"
+
+# Mt. St. Helens crater
+lat = 46.203880
+lon = -122.190498
+
+radius_deg = 0.25
+channel_priorities = ("HHZ", "BHZ")
+
+# How many times to try to download missing traces.
+n_attempts = 5
